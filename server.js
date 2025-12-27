539028291a4e9763ad3d2149eb244899ccd1251f
tcp-git <firefox0x00@gmail.com>
2025-12-27 16:57:09 +0700

    some update

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Load environment variables from .env if present
+require('dotenv').config();
+
 const express = require('express');
 const mysql = require('mysql2/promise');
 const path = require('path');
